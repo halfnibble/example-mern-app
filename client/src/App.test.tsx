a5b07a2ebe885c73f7a0325b089d00605512ae13
Josh Wedekind <josh@halfnibble.com>
2023-10-22 16:57:30 -0700

    Make tests work with axios await

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, act, waitFor, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+import axios from "axios";
+
+jest.mock("axios", () => ({
+    post: jest.fn(),
+    get: jest.fn(),
+}));
+
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+test("renders learn react link", async () => {
+    let app;
+    mockedAxios.get.mockResolvedValue({ data: { time: "07:00:00" } });
+
+    await act(async () => {
+        app = render(<App />);
+    });
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+
+    await waitFor(() => {
+        expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+        expect(screen.getByText(/07:00:00/i)).toBeInTheDocument();
+    });
 });
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
+import axios from "axios";
 import logo from "./logo.svg";
 import "./App.css";
-
-import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
