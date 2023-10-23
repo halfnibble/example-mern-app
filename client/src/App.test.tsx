6fbeaff4f30e07bfd729d096f8eaff56e404108e
Josh Wedekind <josh@halfnibble.com>
2023-10-22 17:56:01 -0700

    Update verions of jest-dom to get types

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { render, act, waitFor, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect"; // Add this line
-import App from "./App";
 import axios from "axios";
+
+import App from "./App";
 
 jest.mock("axios", () => ({
     post: jest.fn(),
